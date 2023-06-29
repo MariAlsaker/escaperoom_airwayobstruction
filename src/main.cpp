4e4b9d1a86944858879a241eb32a86e72ac6b00a
Mari M. Alsaker <mmalsaker@gmail.com>
2023-06-29 14:25:32 +0200

    named the pins

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,20 @@ uint8_t io_state = 0;
 uint32_t pulse_count = 0;
 uint8_t previous_read = 0;
 
+int SENSOR_PIN = 3; 
+int LED = 2; 
+int OUT_PIN = 13;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, OUTPUT);
+  pinMode(OUT_PIN, OUTPUT); // Output to mother board - HIGH if blocked, LOW if not
+  pinMode(SENSOR_PIN, INPUT_PULLUP); // Reading IR sensor
+  pinMode(LED, OUTPUT); // Making LED blink
 
   output_timestamp = millis();
   sampling_timestamp = millis();
   capture_timestamp = millis();
-  //Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() { //This runs all the time at SUPER HIGH speed
@@ -27,16 +31,16 @@ void loop() { //This runs all the time at SUPER HIGH speed
   /* This is a delta capture subroutine, which allows the processor to wait without disrupting other tasks */
   /* Every 20 ms we want to flip the output of the IR LED for it to flash rapidly 
 
-                 10 ms             10 ms
+                 20 ms             20 ms
     LED ON ______      ______      ______      ______      
                  |    |      |    |      |    |      |    |
                  |    |      |    |      |    |      |    |
     LED OFF      ------      ------      ------      ------
   */
-  if(millis() - output_timestamp > 20){ //Every 10 ms
+  if(millis() - output_timestamp > 20){ //Every 20 ms
     output_timestamp = millis(); //Now that the subroutine is activated this is the new relative position we wait from
     io_state = !io_state; //Negate value, turn 0 into 1 and 1 into 0
-    digitalWrite(3, io_state); //Write the negated value to pin 3
+    digitalWrite(LED, io_state); //Write the negated value to pin 3
   }
 
   /* 
@@ -45,10 +49,10 @@ void loop() { //This runs all the time at SUPER HIGH speed
   */
   if(millis() - sampling_timestamp > 1){ //Every 1 ms
     sampling_timestamp = millis();
-    if(digitalRead(2) && (previous_read == LOW)){ //If we see HIGH signal and last time we saw LOW
+    if(digitalRead(SENSOR_PIN) && (previous_read == LOW)){ //If we see HIGH signal and last time we saw LOW
       previous_read = HIGH; //Then we remember that we saw HIGH
       pulse_count++; //We counted one pulse
-    }else if(!digitalRead(2) && (previous_read == HIGH)){ //If we see LOW signal and last time we saw HIGH
+    }else if(!digitalRead(SENSOR_PIN) && (previous_read == HIGH)){ //If we see LOW signal and last time we saw HIGH
       previous_read = LOW; //Then we remember that we saw LOW
     }
   }
@@ -56,11 +60,11 @@ void loop() { //This runs all the time at SUPER HIGH speed
   if(millis() - capture_timestamp > 500){ //Every 500 ms
     capture_timestamp = millis();
 
-    //Serial.println(pulse_count); //We report the pulses we saw
+    Serial.println(pulse_count); //We report the pulses we saw
     if(pulse_count < 2){ //If we saw less than 2 pulses the last 500 ms
-      digitalWrite(13, HIGH); //It's probably blocked
+      digitalWrite(OUT_PIN, HIGH); //It's probably blocked
     }else{ //If not
-      digitalWrite(13, LOW); //It's probably not blocked
+      digitalWrite(OUT_PIN, LOW); //It's probably not blocked
     }
     pulse_count = 0; //Now we have captured for 500 ms, reset count for the next time
   }
